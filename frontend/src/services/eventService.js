470d39527ddb440efc1fc9ea868428782e2cd063
MEET KANANI <mtkanani8776@gmail.com>
2026-07-24 01:16:03 +0530

    Update eventService.js

--- frontend/src/services/eventService.js
+++ frontend/src/services/eventService.js
@@ -1,4 +1,4 @@
-import axiosInstance from '../../services/axiosInstance';
+import axiosInstance from './axiosInstance';
 
 export const getEvents = async (params) => {
   const res = await axiosInstance.get('/api/events', { params, withCredentials: true });
